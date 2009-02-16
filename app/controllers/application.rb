ade7c6912a36c5c74a5fda0f7c4de0ecf4e73b40
mat_aki <matsumura.aki@gmail.com>
2009-02-16 20:46:07 +0900

    add layout and default juggernaut

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -7,9 +7,11 @@ class ApplicationController < ActionController::Base
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery # :secret => 'd6c6817801fac644197b7dc4b97cc0eb'
-  
-  # See ActionController::Base for details 
+
+  # See ActionController::Base for details
   # Uncomment this to filter the contents of submitted sensitive data parameters
-  # from your application log (in this case, all fields with names like "password"). 
+  # from your application log (in this case, all fields with names like "password").
   # filter_parameter_logging :password
+
+  layout 'application'
 end
